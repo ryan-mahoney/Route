cad420e73d76edc2799e468b88cde94230934923
webuser <ryan@virtuecenter.com>
2014-03-08 17:25:56 -0500

    get rid of tabs...

--- src/Route.php
+++ src/Route.php
@@ -25,81 +25,81 @@
 namespace Opine;
 
 class Route {
-	private $collector;
-	private $before = [];
-	private $after = [];
+    private $collector;
+    private $before = [];
+    private $after = [];
 
-	public function __construct ($collector) {
-		$this->collector = $collector;
-	}
+    public function __construct ($collector) {
+        $this->collector = $collector;
+    }
 
-	public function before (callable $callback) {
-		$this->before[] = $callback;
-		return $this;
-	}
+    public function before (callable $callback) {
+        $this->before[] = $callback;
+        return $this;
+    }
 
-	public function after (callable $callback) {
-		$this->after[] = $callback;
-		return $this;
-	}
+    public function after (callable $callback) {
+        $this->after[] = $callback;
+        return $this;
+    }
 
-	public function purgeAfter() {
-		$this->after = [];
-		return $this;
-	}
+    public function purgeAfter() {
+        $this->after = [];
+        return $this;
+    }
 
-	public function purgeBefore () {
-		$this->before = [];
-		return $this;
-	}
+    public function purgeBefore () {
+        $this->before = [];
+        return $this;
+    }
 
-	public function get ($pattern, callable $callback) {
-		$this->method('GET', $pattern, $callback);
-		return $this;
-	}
+    public function get ($pattern, callable $callback) {
+        $this->method('GET', $pattern, $callback);
+        return $this;
+    }
 
-	public function post ($pattern, callable $callback) {
-		$this->method('POST', $pattern, $callback);
-		return $this;
-	}
+    public function post ($pattern, callable $callback) {
+        $this->method('POST', $pattern, $callback);
+        return $this;
+    }
 
-	private function method ($method, $pattern, callable $callback) {
-		$this->collector->addRoute($method, $pattern, $callback);
-	}
+    private function method ($method, $pattern, callable $callback) {
+        $this->collector->addRoute($method, $pattern, $callback);
+    }
 
-	public function run ($method=false, $uri=false, &$header=false) {
-		if ($method === false) {
-			$method = $_SERVER['REQUEST_METHOD'];
-		}
-		if ($uri === false) {
-			$uri = str_replace('?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']);
-	        if (substr_count($uri, '/') > 0) {
-	            $uri = explode('/', trim($uri, '/'))[0];
-	        }
-		}
-    	$dispatcher = new \FastRoute\Dispatcher\GroupCountBased($this->collector->getData());
-		$route = $dispatcher->dispatch($method, $uri);
-		switch ($route[0]) {
-		    case \FastRoute\Dispatcher::NOT_FOUND:
-		        $header = 404;
-		        return false;
+    public function run ($method=false, $uri=false, &$header=false) {
+        if ($method === false) {
+            $method = $_SERVER['REQUEST_METHOD'];
+        }
+        if ($uri === false) {
+            $uri = str_replace('?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']);
+            if (substr_count($uri, '/') > 0) {
+                $uri = explode('/', trim($uri, '/'))[0];
+            }
+        }
+        $dispatcher = new \FastRoute\Dispatcher\GroupCountBased($this->collector->getData());
+        $route = $dispatcher->dispatch($method, $uri);
+        switch ($route[0]) {
+            case \FastRoute\Dispatcher::NOT_FOUND:
+                $header = 404;
+                return false;
 
-		    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-		        $allowedMethods = $route[1];
-		        $header = 405;
-		        return false;
+            case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $route[1];
+                $header = 405;
+                return false;
 
-		    case \FastRoute\Dispatcher::FOUND:
-		        $header =  200;
-		        ob_start();
-		        foreach ($this->before as $before) {
-		        	$before();
-		        }
-		        call_user_func_array($route[1], $route[2]);
-		        foreach ($this->after as $after) {
-		        	$after();
-		        }
-		        return ob_get_clean();
-		}
-	}
+            case \FastRoute\Dispatcher::FOUND:
+                $header =  200;
+                ob_start();
+                foreach ($this->before as $before) {
+                    $before();
+                }
+                call_user_func_array($route[1], $route[2]);
+                foreach ($this->after as $after) {
+                    $after();
+                }
+                return ob_get_clean();
+        }
+    }
 }
